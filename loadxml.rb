e2d6c53bb52939777c6e00d5a33837dbabb2240b
Frank Jung <frankhjung@linux.com>
2014-09-11 23:00:18 +1000

    use printf for complex formatted strings, reformatted using rbeautify

--- loadxml.rb
+++ loadxml.rb
@@ -10,61 +10,62 @@ require 'xmlsimple'
 #
 
 opts = Trollop::options do
-    version "#{File.basename($0)} 0.0.1 © 2013 Frank H Jung"
-    banner <<-USAGE
-    
-NAME
+  version "#{File.basename($0)} 0.0.1 © 2013 Frank H Jung"
+  banner <<-USAGE
 
-    #{File.basename($0)} - a Ruby example program to show XML processing
+  NAME
 
-SYNOPSIS
+  #{File.basename($0)} - a Ruby example program to show XML processing
 
-    #{File.basename($0)} [options] <filename>
+  SYNOPSIS
 
-OPTIONS
-    
-USAGE
-    opt :verbose, "Verbose output", :default => false, :short => '-v'
-    opt :file, "XML filename to read", :default => 'test.xml', :short => '-f', :type => String
+  #{File.basename($0)} [options] <filename>
+
+  OPTIONS
+
+  USAGE
+  opt :verbose, "Verbose output", :default => false, :short => '-v'
+  opt :file, "XML filename to read", :default => 'test.xml', :short => '-f', :type => String
 end
 
 Trollop::die :file, "must exist" unless File.exist?(opts[:file]) if opts[:file]
 
 #
 # MAIN
-# 
+#
 
 # read in xml document
-companies = XmlSimple.xml_in(opts[:file], { 
-    'KeyAttr' => 'name',
-    'ForceArray' => [ 'year' ],
-    'ContentKey' => '-content' 
-})
+companies = XmlSimple.xml_in(opts[:file], {
+                               'KeyAttr' => 'name',
+                               'ForceArray' => [ 'year' ],
+                               'ContentKey' => '-content' })
 
 # show a specific year
 puts "(1) show a specific year\n"
 turnover = companies['turnover']['year'].find { |year| year['id'] == "2012"}
-puts "\tYear 2012 has turnover of #{turnover['content']}\n\n"
+printf "\tYear 2012 has turnover of %s\n\n", turnover['content']
 
 # show all years
 puts "(2) show all years\n"
-companies['turnover']['year'].each { |year| 
-    puts "\tYear #{year['id']} has turnover of #{year['content']}\n"
+companies['turnover']['year'].each { |year|
+  printf "\tYear %s has turnover of %s\n", year['id'], year['content']
 }
 puts "\n"
 
 #  dump xml file
 if opts[:verbose]
-    puts "(3) dump XML file\n"
-    File.open(opts[:file], 'r') do | xmlDoc |
-        puts "\tContents of #{opts[:file]}:"
-        xmlDoc.each_line { |line| puts "\t#{line}" }
-    end
-    puts "\n"
+  puts "(3) dump XML file\n"
+  File.open(opts[:file], 'r') do | xmlDoc |
+    printf "\tContents of %s:\n", opts[:file]
+    xmlDoc.each_line { |line| printf "\t%s", line}
+  end
+  puts "\n"
 end
 
 # dump xml document
 if opts[:verbose]
-    puts "(4) Dump the XML that was read\n"
-    puts JSON.pretty_generate(companies)
+  puts "(4) Dump the XML that was read\n"
+  puts JSON.pretty_generate(companies)
 end
+
+#EOF
