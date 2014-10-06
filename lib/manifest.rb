c9832578347a2e1a2df889a35f918b5071286c01
Frank Jung <frankhjung@linux.com>
2014-10-06 18:51:18 +1100

    process maven manifest using REXML:XPath

---
+++ lib/manifest.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'rexml/document'
+require 'rexml/xpath'
+
+#
+# Process a Maven manifest to version information for this artefact.
+#
+# Author:: Frank Jung
+# License:: see LICENSE
+#
+# = References
+#
+# * REXML
+#
+#   http://ruby-doc.org/stdlib-1.9.3/libdoc/rexml/rdoc/REXML.html
+#
+class Manifest
+  include REXML
+  # Default contructor with source XML containing employees data
+  def initialize
+    @doc = nil
+  end
+
+  # Load versions from maven manifest (XML) document
+  def load(file)
+    fail unless File.exist?(file)
+    @doc = Document.new(File.new file)
+    fail unless @doc
+  end
+
+  # Retrieve latest version from manifest
+  def latest
+    latest = XPath.first(@doc, '/metadata/versioning/latest')
+    return unless latest
+    latest.text
+  end
+
+  # Retrieve release version from manifest
+  def release
+    release = XPath.first(@doc, '/metadata/versioning/release')
+    return unless release
+    release.text
+  end
+
+  # Retrieve a list of versions from manifest
+  def versions
+    versions = XPath.each(@doc, '/metadata/versioning/versions/version')
+    return unless versions
+    versions.map(&:text)
+  end
+end
---
+++ tests/test_manifest.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require_relative '../lib/manifest'
+require 'minitest'
+require 'minitest/autorun'
+
+# Test manifest - a class to read and process an XML document.
+#
+# Author:: Frank Jung
+# License:: see LICENSE
+#
+# = References
+#
+# * Tested with:
+#
+#   ruby 1.9.3p484 (2013-11-22 revision 43786) [x86_64-linux].
+#
+# * MiniTest:
+#
+#   https://github.com/seattlerb/minitest
+#
+class TestManifest < Minitest::Test
+  # create manifest class and load test file
+  def setup
+    @manifest = Manifest.new
+    assert_instance_of(Manifest, @manifest)
+    file = File.expand_path('../data/maven-metadata.xml',
+                            File.dirname(__FILE__))
+    @manifest.load(File.new file)
+  end
+
+  # load of bad file
+  def test_bad_file
+    file = 'bad_file'
+    assert_raises RuntimeError do
+      @manifest.load(file)
+    end
+  end
+
+  # retrieve latest version from manifest
+  def test_latest
+    assert_equal('2.3.0', @manifest.latest)
+    refute_kind_of(REXML::Element, @manifest.latest)
+    assert_kind_of(String, @manifest.latest)
+  end
+
+  # retrieve release version from manifest
+  def test_release
+    assert_equal('2.3.0', @manifest.release)
+    refute_kind_of(REXML::Element, @manifest.release)
+    assert_kind_of(String, @manifest.release)
+  end
+
+  # retrieve list of versions from manifest
+  def test_versions
+    versions = @manifest.versions
+    refute_nil(versions)
+    assert_equal(13, versions.length)
+    assert_equal('0.4.0', versions.sort[0])
+    assert_equal('2.3.0', versions.sort[-1])
+    assert_includes(versions, '1.3.2')
+  end
+end
